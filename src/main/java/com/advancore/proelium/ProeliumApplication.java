03bc309efdb2f38e532eacf8f1c094a9dc7670de
Jhon Carlos Acevedo Mendoza <jhoncarlosacevedomendoza@gmail.com>
2023-10-06 18:26:56 -0500

    project generated

---
+++ src/main/java/com/advancore/proelium/ProeliumApplication.java
@@ -0,0 +1,13 @@
+package com.advancore.proelium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProeliumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProeliumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/advancore/proelium/ProeliumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advancore.proelium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProeliumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
